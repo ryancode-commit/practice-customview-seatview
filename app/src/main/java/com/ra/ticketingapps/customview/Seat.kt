5aa5815296daa27881551351e69728be9fd1e6dc
Ryan Ardyansyah <ui.ardyan@gmail.com>
2022-12-04 21:08:26 +0700

    Initial commit

---
+++ app/src/main/java/com/ra/ticketingapps/customview/Seat.kt
@@ -0,0 +1,9 @@
+package com.ra.ticketingapps.customview
+
+data class Seat(
+    val id: Int,
+    var x : Float? = 0F,
+    var y : Float? = 0F,
+    var name : String,
+    var isBooked : Boolean
+)
